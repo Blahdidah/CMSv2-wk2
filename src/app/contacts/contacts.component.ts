2cc44cfaa6f7bd3091103c80d1016bad22f83917
Blahdidah <80852722+Blahdidah@users.noreply.github.com>
2025-01-18 19:57:21 -0700

    Someday I'll find a better way to do this.
    all my files!

---
+++ src/app/contacts/contacts.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Contact } from './contact.model';
+
+@Component({
+  selector: 'cms-contacts',
+  standalone: false,
+
+  templateUrl: './contacts.component.html',
+
+})
+export class ContactsComponent {
+  
+}
